473c5198b5f2f80b550a4536bd045de5e7195e34
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-01-17 16:43:44 +0100

    Upd: preparefile for postgres

--- scripts/network/backend/backend_postgres.gd
+++ scripts/network/backend/backend_postgres.gd
@@ -1,4 +1,4 @@
-extends BackendBase
+class_name BackendPostgres extends BackendBase
 
 # .env
 # Only required if backend is "postgres"
@@ -9,15 +9,6 @@ extends BackendBase
 # POSTGRES_DB=jdungeon
 
 #var backen_initialized = false
-const BACKEND_CONFIG={
-	"POSTGRESS": {
-		"Address": "127.0.0.1",
-		"Port": 5432,
-		"User": "testuser",
-		"Passwd": "testpasswd",
-		"DB": "jdungeon"
-	}
-}
 
 
 func _init() -> void:
