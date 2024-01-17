078d6a6461135c9868744a64e3b2d5280d0de0da
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-01-17 23:11:03 +0100

    Add: schema from postgres as help Add: define the user table

--- scripts/network/backend/backend_sqlite.gd
+++ scripts/network/backend/backend_sqlite.gd
@@ -31,13 +31,42 @@ func create_backend_if_not_exists(_path: String, _json_data: Dictionary) -> bool
 
 	## check for database
 	## - create if needed
+
+	#CREATE TABLE users (
+	#id SERIAL PRIMARY KEY,
+	#username VARCHAR(255) NOT NULL UNIQUE,
+	#password VARCHAR(255) NOT NULL,
+	#data json DEFAULT '{}'
+	#);
+
+
 	table_dictionary["id"] = {
-	"data_type":"int",
-	"primary_key": true,
-	"auto_increment":true
+		"data_type":"int",
+		"primary_key": true,
+		"auto_increment":true
+	}
+	table_dictionary["username"] = {
+		"data_type":"char(255)",
+		"not_null": true,
+		"unique": true,
+	}
+	table_dictionary["password"] = {
+		"data_type":"char(255)",
+		"not_null": true,
+		"unique": false,
+	}
+	table_dictionary["data"] = {
+		"data_type":"text",
+		"not_null": true,
+		"unique": false,
 	}
 
 	## check for tabel(s)
 	## - create if needed
+	# var success = create_table( "users", table_dictionary )
+
 	db.close_db()
 	return success
+
+
+# db.query_with_bindings("UPDATE ? SET ?=? WHERE id=?;", [table_name, column_name, 100, 1])
