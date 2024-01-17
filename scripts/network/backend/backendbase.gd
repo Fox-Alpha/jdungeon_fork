aa62012dfbcbcbf0d00bc934782476cf87c7023b
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-01-17 16:47:56 +0100

    Add: Get Backend_Config from env_variable

--- scripts/network/backend/backendbase.gd
+++ scripts/network/backend/backendbase.gd
@@ -1,11 +1,16 @@
 class_name BackendBase extends Node
 
+# TODO: globalise path
+const DEFAULT_DB_FILE_PATH: String = "user://"
+const DEBUG_DB_FILE_PATH: String = "res://"
+
 var backend_initialized = false
 
-var BACKEND_CONFIG=""
+var BACKEND_CONFIG: Dictionary = {}
 
 func _init() -> void:
-	pass
+	#var _error = Global.load_database_env_variables()
+	BACKEND_CONFIG = Global.env_backend_config
 
 
 func create_backend_if_not_exists(_path: String, _json_data: Dictionary) -> bool:
